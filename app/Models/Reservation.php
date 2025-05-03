340b4395839fe82c686f89b97bbe6466365d3913
ElFirdaous28 <elmokhtarifirdaous@gmail.com>
2025-05-03 13:30:04 +0100

    strip payment remove

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -26,11 +26,6 @@ class Reservation extends Model
         return $this->belongsTo(User::class);
     }
 
-    public function payment()
-    {
-        return $this->hasOne(Payment::class);
-    }
-
     public function review()
     {
         return $this->hasOne(Review::class);
